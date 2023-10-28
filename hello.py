3fceeb2ae9905a2e8b8dffaf3e5ee87b92154316
22p31f0051 <pvsivadurgapilli@gmail.com>
2023-10-28 18:07:28 +0530

    28OCT23

---
+++ hello.py
@@ -0,0 +1,8 @@
+print("hello world")
+a=19
+b=1999
+print(a)
+print(b)
+list1=["maha",29,"mca",1.25]
+print(list[1:])
+print(list[2])
---
+++ list.py.py
@@ -0,0 +1,15 @@
+print("hello world")
+a=19
+b=1999
+print(a)
+print(b)
+print(a+b)
+print(a-b)
+print(a//b)
+print(a*b)
+a="mahalakshmi"
+print(a)
+print(a[5:])
+list1=[1,"hello",400,"word"]
+print(list1[2:])
+print(list1*3)
