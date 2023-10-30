65b71168bbb3e76b590834fc1a557d375472c3b0
Narayana <peketilakshminarayana17@gmail.com>
2023-10-30 22:08:27 +0530

    30OCT23

--- web/urls.py
+++ web/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from products.views import productlist
+from Home.views import Home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('welcome/',productlist),
+    path('',Home),
 ]
