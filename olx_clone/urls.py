8040b785a5597758708712a584ad9e26e9ec862f
Ajay Reddy <ajayreddy3812@gmail.com>
2023-10-30 20:45:09 +0530

    task2/home/30OCT23

--- olx_clone/urls.py
+++ olx_clone/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from products.views import ProductList
+from home.views import Home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',Home),
     path('products/',ProductList)
 ]
