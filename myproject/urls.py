54d4db8196067eeea98ce70c802033f9c98028e8
Chaitu0147 <chaitu0147@gmail.com>
2023-10-31 20:10:10 +0530

    31OCT23

--- myproject/urls.py
+++ myproject/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from myapp1.views import app
+from myapp1.views import app,chai
 from myapp.views import myapp
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('app/',app),
-    path('myapp/',myapp)
+    path('myapp/',myapp),
+    path('html/',chai)
 ]
