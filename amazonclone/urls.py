1ad30be346e5db41e159fc699ac5bc32f7e12c37
krishna gangumalla <22p31f0016@gmail.com>
2023-10-31 13:02:59 +0530

    31OCT23

--- amazonclone/urls.py
+++ amazonclone/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from products.views import products
+from home.views import home
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', products),
+    path('home1/',home),
 ]
---
+++ products/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+#eate your views here.
+def products(request):
+    return HttpResponse('output')
