291927b627b36eee2e67e36233573c7f1a171634
22P31F0014 <22P31F0014@acet.ac.in>
2023-10-31 15:29:17 +0530

    31OCT2023

--- olx_clone/urls.py
+++ olx_clone/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from products.views import ProductList
+from home.views import Home
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('products/', ProductList),
+    path('home/', Home),
 ]
