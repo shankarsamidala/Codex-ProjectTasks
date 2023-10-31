0e1887d180aec3281d93f00ef9d56685662c6bb7
22a91f0014 <22a91f0014>
2023-10-31 13:19:32 +0530

    31oct2023

---
+++ home/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def HomePage(request):
+    return HttpResponse('Welcome to Home Page')
+
+
+
