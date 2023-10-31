a46561f75ff10b6e86cf556714cb1849e8913f70
22P31F0020 <22p31f0020@acet.ac.in>
2023-10-31 11:25:10 +0530

    task2/31oct23

--- home/views.py
+++ home/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
 # Create your views here.
-
-def Products(request):
-    return HttpResponse('Product Page')
+def homepage(request):
+    return HttpResponse('HomePage')
