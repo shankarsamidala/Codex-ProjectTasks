170ab5502a5bb2b17e72b8cc8e0bbe977ee0529f
vineetha veeramalla <22p31f0064@acet.ac.in>
2023-10-31 07:52:20 +0530

    30OCT2023

--- olx_clone/urls.py
+++ olx_clone/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from products.views import ProductList
+from home.views import Home
+from orders.views import OrderList
 urlpatterns = [
     path('admin/', admin.site.urls),
+     path('', Home),
+    path('products/', ProductList),
+    path('orders/', OrderList),
+   
 ]
