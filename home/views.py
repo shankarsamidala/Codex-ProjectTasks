3e288a62d79307c852dcd32a2e0dd2dee7dbecb3
22a91f0055 <22a91f0055>
2023-10-31 10:50:25 +0530

    31oct2023

--- home/views.py
+++ home/views.py
@@ -2,7 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-
-def products(request):
-    return HttpResponse(' welcome to products page')
-
+def home(request):
+    return HttpResponse('welcome to home page')
