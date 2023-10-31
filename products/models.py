88ab87299eaa640a4577e0e7f77454a11d7c5b5f
Hema028 <22p31f0028@acet.ac.in>
2023-10-31 22:06:48 +0530

    task3/31oct23

--- products/models.py
+++ products/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class ProductList(models.Model):
+    name = models.CharField(max_length=250)
+    price = models.IntegerField()
+    tag = models.CharField(max_length=250)
+    rating = models.IntegerField()
+    created_at = datetime.now()
+
+    
