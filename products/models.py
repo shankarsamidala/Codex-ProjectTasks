2002bf0de09b796e5cb0a98997e770e3cf387cde
Tejakandapu <22A91F0021@aec.edu.in>
2023-11-01 19:19:19 +0530

    01NOV23

--- products/models.py
+++ products/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+class productsList(models.Model):
+    title=models.CharField(max_length=250)
+    price=models.IntegerField(max_length=250)
+    tag=models.CharField(max_length=250)
+    rating=models.IntegerField(max_length=250)
+    create_at=datetime.now()
--- products/views.py
+++ products/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def ProductPage(request):
-    return HttpResponse('products page')
+    return render(request,'products.html')
 
