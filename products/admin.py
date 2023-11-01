7ed8e17ea6de1e65e01011244cc84e13c4bb3b4a
22p31f0039 <you@example.com>
2023-11-01 16:35:29 +0530

    1nov23

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import productlist
+admin.site.register(productlist)
 # Register your models here.
--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import productlist
 # Create your views here.
 def productlist(request):
-    return HttpResponse('product page')
+    return render(request,'products.html')
