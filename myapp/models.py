1f3fa940ef43ba999c1bda1cd68b933a038fbdfa
Chaitu0147 <chaitu0147@gmail.com>
2023-11-01 18:19:10 +0530

    01nov23

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+
+
+class ProductsList(models.Model):
+    name = models.CharField(max_length=250)
+    tag = models.CharField(max_length=250)
+    price = models.IntegerField()
+    rating = models.IntegerField(default=0)
+    created_at = datetime.now()
