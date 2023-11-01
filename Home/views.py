bb78ddb13a3f69d894249982bd2833ea947b339a
Nishitha Yanduri <22A91F0064>
2023-11-01 13:09:03 +0530

    1Nov2023

--- Home/views.py
+++ Home/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def Homepage(request):
-    return HttpResponse('Home page')
+    return render(request,'home.html')
--- products/views.py
+++ products/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def Product(request):
-    return HttpResponse('Products page')
+    return render(request,'products.html')
