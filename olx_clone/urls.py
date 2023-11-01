bbed8d46d996025f7963788562f7d8213ca29605
HarshaKandula1707 <22A91F0022@aec.edu.in>
2023-11-01 13:41:27 +0530

    1NOV23

--- olx_clone/urls.py
+++ olx_clone/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from products.views import Products
+from homepage.views import Home
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('Products/',Products),
+    path('',Home)
 ]
+
