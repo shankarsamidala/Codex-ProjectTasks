13f8f6ab272dcabc09fc04583a3a7061d038f2c0
HarshaKandula1707 <22A91F0022>
2023-11-01 18:15:10 +0530

    1NOV23

--- products/models.py
+++ products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class productsList(models.Model):
+    title=models.CharField(max_length=250)
+    price=models.IntegerField(max_length=250)
+    Tag=models.CharField(max_length=250)
+    Rating=models.IntegerField(max_length=250)
+    create_at=datetime.now()
+
