7d2da9430239329f5cd6e010df735451c593abc5
22P31F0042 <22p31f0042@acet.ac.in>
2023-11-01 23:01:34 +0530

    01NOV2023

--- products/views.py
+++ products/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import ProductsList
 # Create your views here.
 
 def ProductList(request):
-    return HttpResponse('Products Page Sample')
+    data=ProductsList.objects.all()
+    
+    #print('Data from Products table',data)
+    
+    #for i in data:
+    #    if i.price >= 25000:
+    #       print(i.price)
+    
+    products = {
+       'data' : data
+    }
+    
+    return render(request,'products.html',context=products)
