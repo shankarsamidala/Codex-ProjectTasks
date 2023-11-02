0ce5fac1e0c47a1ceb917a9bc81c9aab1fc885fc
22a91f0033 <22a91f0033@aec.edu.in>
2023-11-02 10:59:01 +0530

    02nov203

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from.models import productslist
 
 # Register your models here.
+
+admin.site.register(productslist)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class productslist(models.Model):
+    title=models.CharField(max_length=250)
+    price=models.IntegerField(default=0)
+    tag=models.CharField(max_length=250)
+    rating=models.CharField(max_length=250)
+    model=models.CharField(max_length=250,default=0)
