bc3193c0d394619b1410a7eea9e29f523995bb0d
22p31f0059 <madhuricherry215@gmail.com>
2023-11-02 17:18:16 +0530

    2nov23

--- products/models.py
+++ products/models.py
@@ -11,5 +11,7 @@ class ProductList(models.Model):
     tag = models.CharField(max_length=250)
     price = models.IntegerField()
     rating = models.IntegerField()
-    created_at = datetime.now()
- 
+    created_at = models.DateTimeField(default=datetime.now)
+
+    def __str__(self):
+     return self.title
--- products/views.py
+++ products/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from.models import ProductList
 
 
 # Create your views here
 
 def ProductsList(request):
-    return render(request,'products.html')
+
+    data = ProductList.objects.all()
+    print('Data From Product Table',data)
+    content={
+        'data':data
+    }
+    return render(request,'products.html',context=content)
 
