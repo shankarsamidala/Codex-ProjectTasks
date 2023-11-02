f12d42cd2ca46a3c16a198c37c1c8909a3b30da1
22a91f0026 <22a91f0026>
2023-11-02 12:13:28 +0530

    2nov23

--- products/models.py
+++ products/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class productList(models.Model):
+    title = models.CharField(max_length=250)
+    price = models.IntegerField(default=0)
+    tag = models.CharField(max_length=250)
+    rating = models.CharField(max_length=250)
+    modelname=models.CharField(max_length=250,default=0)
+
