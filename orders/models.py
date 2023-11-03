9e08599e8ceec33882bdb92e2d56d082de3711bd
22p31f0051 <pvsivadurgapilli@gmail.com>
2023-11-03 08:56:43 +0530

    03NOV23

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from datetime import datetime
+from products.models import ProductList
 
 # Create your models here.
+
+class orders_list(models.Model):
+    product= models.OneToOneField(ProductList,on_delete=models.CASCADE,default=1)
+
+    def __str__(self):
+        return self.product.title
--- orders/views.py
+++ orders/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import orders_list
 
 
 # Create your views here.
 
 def orders(request):
-    return HttpResponse('orders')
+    data=orders_list.objects.all()
+    print('Data from Products Table',data)
+
+    prd={
+        'data':data
+    }
+    return render(request,'orders.html',context=prd)
