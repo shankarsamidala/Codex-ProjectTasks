eb15f8f46b53736f505ca18a432dced356e09762
Samhitha4014 <22A91F0025@aec.edu.in>
2023-11-03 09:36:49 +0530

    03NOV23

--- products/models.py
+++ products/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class productsList(models.Model):
+    title=models.CharField(max_length=250)
+    price=models.IntegerField(default=0)
+    tag=models.CharField(max_length=250)
+    Rating=models.IntegerField(default=0)
+    create_at=datetime.now()
--- products/views.py
+++ products/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+
 def product(request):
-    return HttpResponse('productpage')
+    return render(request,'products.html')
 
 # Create your views here.
