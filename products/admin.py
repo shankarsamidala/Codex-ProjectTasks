758412b673b51eb0baf018fffd8d85ec25f92a8e
Satyasreekurukuri <22p31f0033@acet.ac.in>
2023-11-06 13:37:08 +0530

    task4/6 Nov 2023

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from.models import productslist
 # Register your models here.
+admin.site.register(productslist)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class productslist(models.Model):
+    title=models.CharField(max_length=250)
+    price=models.IntegerField(default=0)
+    tag=models.CharField(max_length=250)
+    rating=models.IntegerField(default=0)
