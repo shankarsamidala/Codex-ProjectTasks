6ebe592d03c896e1e71e1d00d849d0191a3fcf56
22P31F0042 <22p31f0042@acet.ac.in>
2023-11-06 17:07:42 +0530

    06NOV2023

--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+#from django.shortcuts import render
+#from django.http import HttpResponse
 
 # Create your views here.
 
-def OrderList(request):
-    return HttpResponse('Orders List')
+#def OrderList(request):
+#    return HttpResponse('Orders List')
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import OrderList
+# Create your views here.
+
+def Orders(request):
+    data=OrderList.objects.all()
+    
+    #print('Data from Products table',data)
+    
+    #for i in data:
+    #    if i.price >= 25000:
+    #       print(i.price)
+    
+    orders = {
+       'data' : data
+    }
+    print('data from products table',data)
+    return render(request,'orders.html',context=orders)
