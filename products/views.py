6400c75aafcbc70fead8d1f7f701a011873a7495
22a91f0055 <22a91f0055>
2023-11-06 14:28:16 +0530

    6nov2023

--- products/views.py
+++ products/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import productList
 # Create your views here.
 
 def products (request):
-    return render(request,'products.html')
+
+    data = productList.objects.all()
+
+    print(data)
+
+    products = {
+        'data':data
+    }
+
+    return render(request,'products.html',context=products)
 
