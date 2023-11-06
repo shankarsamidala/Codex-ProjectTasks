6b80408c89820d907b5dace4c9ffd250af02cbcf
vineetha veeramalla <22p31f0064@acet.ac.in>
2023-11-06 17:06:47 +0530

    06NOV2023

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from products.models import ProductsList
 
 # Create your models here.
+class OrderList(models.Model):
+    Product= models.OneToOneField(ProductsList,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.Product.title
--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import OrderList
 # Create your views here.
 
-def OrderList(request):
-    return HttpResponse('Orders page')
+def Orders(request):
+    data=OrderList.objects.all()
+    orders = {
+        'data':data
+    }
+    print('data from products table',data)
+    return render(request,'orders.html',context=orders)
